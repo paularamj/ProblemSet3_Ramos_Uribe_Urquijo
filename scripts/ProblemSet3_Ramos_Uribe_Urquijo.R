1950e9c0caa08d508d71e239fe8d667d9ad0ba50
Paula Ramos <p.ramosj@uniandes.edu.co>
2022-07-20 21:14:55 -0500

    Importar datos manzanas DANE

--- scripts/ProblemSet3_Ramos_Uribe_Urquijo.R
+++ scripts/ProblemSet3_Ramos_Uribe_Urquijo.R
@@ -38,7 +38,7 @@ db<- st_as_sf(x=train,coords=c("lon","lat"),crs=4326) ##Lectura de datos espacia
 leaflet() %>% addTiles() %>% addCircles(data=db)
 class(db)
 
-##Chapinero
+##Caja de coordenada que contiene el polígono de Chapinero - Bogotá
 chapinero <- getbb(place_name = "UPZ Chapinero, Bogota", 
                    featuretype = "boundary:administrative", 
                    format_out = "sf_polygon") %>% .$multipolygon
@@ -46,79 +46,113 @@ chapinero <- getbb(place_name = "UPZ Chapinero, Bogota",
 leaflet() %>% addTiles() %>% addPolygons(data=chapinero)
 train_chapinero <- st_crop(db, chapinero)
 
-##Poblado
+##Caja de coordenada que contiene el polígono de Poblado - Medellín
 poblado <- getbb(place_name = "Comuna 14 - El Poblado", 
                  featuretype = "boundary:administrative", 
                  format_out = "sf_polygon") 
 leaflet() %>% addTiles() %>% addPolygons(data=poblado)
 train_poblado <- st_crop (db, poblado)
 
-available_features() #Escoger features 
-
 ######Variables de OSM######
+available_features() #Escoger features
 
-###Variable: Bares
-
+###Variable: Bares##
+#Chapinero
 bar_chap <- opq(bbox = st_bbox(chapinero)) %>%
   add_osm_feature(key = "amenity", value = "bar")
-class(bar_chap)
 osm_bar_chap<- bar_chap %>% osmdata_sf()
-##points
-bares_chapinero <- osm_bar$osm_points %>% select(osm_id,amenity) 
-class(bares_chapinero)
-
+bares_chapinero <- osm_bar$osm_points %>% select(osm_id,amenity) ##points
+#Poblado
 bar_pob <- opq(bbox = st_bbox(poblado)) %>%
   add_osm_feature(key = "amenity", value = "bar")
-class(bar_pob)
 osm_bar_pob<- bar_pob %>% osmdata_sf()
-##points
-bares_poblado <- osm_bar_pob$osm_points %>% select(osm_id,amenity) 
-class(bares_poblado)
+bares_poblado <- osm_bar_pob$osm_points %>% select(osm_id,amenity)  ##points
 
 ###Variable: Estaciones de bus
+#Chapinero
 est_bus_chap <- opq(bbox = st_bbox(chapinero)) %>%
   add_osm_feature(key = "amenity", value = "bus_station")
-class(est_bus_chap)
 #lista de los objetos
 osm_eb_chap<- est_bus_chap%>% osmdata_sf()
-#points
-estaciones_chapinero <- osm_eb_chap$osm_points %>% select(osm_id,amenity) 
+estaciones_chapinero <- osm_eb_chap$osm_points %>% select(osm_id,amenity) #points
 
+#Poblado
 est_bus_pob <- opq(bbox = st_bbox(poblado)) %>%
   add_osm_feature(key = "amenity", value = "bus_station")
-class(est_bus_pob)
 osm_eb_pob<- est_bus_pob%>% osmdata_sf()
-#points
-estaciones_poblado <- osm_eb_pob$osm_points %>% select(osm_id,amenity) 
+estaciones_poblado <- osm_eb_pob$osm_points %>% select(osm_id,amenity) #points
 
 ###Variable: Parques
-
+#Chapinero
 parques_chap <- opq(bbox = st_bbox(chapinero)) %>%
   add_osm_feature(key = "leisure", value = "park")
 osm_park_cha<- parques_chap %>% osmdata_sf()
-#poligonos
-parques_chapinero<- osm_park_cha$osm_polygons %>% select(osm_id,leisure) 
+parques_chapinero<- osm_park_cha$osm_polygons %>% select(osm_id,leisure) #poligonos 
 
+#Poblado
 parques_pob <- opq(bbox = st_bbox(poblado)) %>%
   add_osm_feature(key = "leisure", value = "park")
-osm_park_pob<- parques_pob %>% osmdata_sf()
-#poligonos
-parques_pob <- osm_park_pob$osm_polygons %>% select(osm_id,leisure) 
-
+osm_park_pob<- parques_pob %>% osmdata_sf() #lista de elementos (points, lines, poligonos)
+parques_pob <- osm_park_pob$osm_polygons %>% select(osm_id,leisure) #poligonos
+ 
 ###Variable: Bancos
-bancos = opq(bbox = st_bbox(Pol_db)) %>%
+#Chapinero
+ban_chp = opq(bbox = st_bbox(chapinero)) %>%
   add_osm_feature(key = "amenity", value = "bank")
+osm_ban_chap<- ban_chp  %>% osmdata_sf() #lista de elementos (points, lines, poligonos)
+bancos_chapinero <- osm_ban_chap$osm_points %>% select(osm_id,amenity) #points
 
-osm_bks<- bancos %>% osmdata_sf()
-banks <- osm_bks$osm_points %>% select(osm_id,amenity) 
+#Poblado
+ban_pob = opq(bbox = st_bbox(poblado)) %>%
+  add_osm_feature(key = "amenity", value = "bank")
+osm_ban_pob<- ban_pob %>% osmdata_sf() #lista de elementos (points, lines, poligonos)
+bancos_poblado <- osm_ban_pob$osm_points %>% select(osm_id,amenity)  #points
 
 ##Inspección gráfica
 leaflet() %>% addTiles() %>% 
   addCircleMarkers(data=db , col="red", weight=2)%>% #datos
-  addPolygons(data=parks , col="green") %>%  #parques
-  addCircles(data=bares , col="blue") %>%  #bares
-  addCircles(data=banks , col="black" , weight=2)%>% # bancos
-  addCircles(data=bus_station , col="yellow")  #estaciones de bus
+  addPolygons(data=parques_chapinero , col="green") %>%  #parques
+  addCircles(data=bares_chapinero , col="blue") %>%  #bares
+  addCircles(data=bancos_chapinero , col="black" , weight=2)%>% # bancos
+  addCircles(data=estaciones_chapinero , col="yellow") %>%   #estaciones de bus
+  addPolygons(data=parques_pob , col="green") %>%  #parques
+  addCircles(data=bares_poblado , col="blue") %>%  #bares
+  addCircles(data=bancos_poblado , col="black" , weight=2)%>% # bancos
+  addCircles(data=estaciones_poblado , col="yellow")  #estaciones de bus
+
+##Lectura de archivos shp del DANE
+Bogota_mzn<- st_read("C:/Users/pau_9/Downloads/MGN2017_11_BOGOTA/11_BOGOTA/URBANO/MGN_URB_MANZANA.shp")
+Antioquia_mzn<- st_read("C:/Users/pau_9/Downloads/MGN2017_05_ANTIOQUIA/05_ANTIOQUIA/URBANO/MGN_URB_MANZANA.shp")
+######Nota: Deje la ruta de desacargas porque no me deja vincularlo al shp file en nuestra carpeta de github
+
+
+##Afinar las transformaciones
+st_crs(Bogota_mzn) == st_crs(train_chapinero)
+st_crs(Antioquia_mzn) == st_crs(train_poblado)
+
+##Unir dos conjuntos de datos basados en la geometría
+housing_chapinero = st_join(x=train_chapinero , y=Bogota_mzn)
+housing_poblado = st_join(x=train_poblado , y=Antioquia_mzn)
+
+##Precio promedio
+max_price = st_join(x=st_buffer(x=train_chapinero, dist=50) , y=Bogota_mzn)
+st_geometry(max_price) = NULL
+
+
+
+
+##Variable: Distancia a bares
+
+dist_bar = st_distance(x=housing_p , y=bar)
+
+
+
+##Variable: Distancia a parques 
+
+
+##Variable: Distancia a bancos
+
+
 
 ##Variables de description
 
